16247822efba9b7fb55884d5a97a8c14213e6f03
Clarice Silber <cssilber@Clarices-MacBook-Pro.local>
2016-04-20 17:13:15 -0400

    Bot commit

--- tweetbot2.py
+++ tweetbot2.py
@@ -1,14 +1,14 @@
 from twython import Twython
 import csv
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = 'Janbon4bSacjLP2RUVeHz2UoY'
+CONSUMER_SECRET = 'ttAnmMWLy1GsbUCdkZY2unCP1VgD1eMWIpOQQtO387Yt6uRa2P'
+ACCESS_TOKEN = '722878958702690304-A5m3Zpyk6tX7GVIzEasaBtQrz7WrTn8'
+ACCESS_TOKEN_SECRET = '3wxSmW0ezDKSyFTrt1uVvacctb6uFDx9BVXkCkYcNZvMb'
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-search = twitter.search(q='propublica', count="100")
+search = twitter.search(q='seltzer', count="100")
 tweets = search['statuses']
 
 with open ('data.csv', 'w') as fp:
@@ -25,5 +25,5 @@ with open ('data.csv', 'w') as fp:
             url = result['entities']['urls'][0]['expanded_url']
         except:
             url = None
-        text=[['propublica', result['text'].encode('utf-8'), url]]
+        text=[['seltzer', result['text'].encode('utf-8'), url]]
         a.writerows((text))
